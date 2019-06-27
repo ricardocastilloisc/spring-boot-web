07c3282c61880782c7601b51806a154d4af50ca6
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-06-27 16:04:54 -0500

    025 Inyectar valores usando la anotacion Value

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -1,9 +1,10 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
-import java.util.ArrayList;
+
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,10 +16,19 @@ import com.bolsadeideas.springboot.web.app.models.Usuario;
 @Controller
 @RequestMapping("/app")
 public class IndexController {
+	
+	@Value("${texto.indexcontroller.index.titulo}")
+	private String textoIndex;
+	
+	@Value("${texto.indexcontroller.perfil.titulo}")
+	private String textoPerfil;
+	
+	@Value("${texto.indexcontroller.listar.titulo}")
+	private String textoListar;
 
 	@GetMapping({ "/index", "/", "/home" })
 	public String index(Model model) {
-		model.addAttribute("titulo", "Hola Spring Framework con Model");
+		model.addAttribute("titulo", textoIndex);
 		return "index";
 	}
 
@@ -29,13 +39,13 @@ public class IndexController {
 		usuario.setApellido("Guzmán");
 		usuario.setEmail("hola@gmail.com");
 		model.addAttribute("usuario", usuario);
-		model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+		model.addAttribute("titulo", textoPerfil.concat(usuario.getNombre()));
 		return "perfil";
 	}
 
 	@RequestMapping("/listar")
 	public String listar(Model model) {
-		model.addAttribute("titulo", "Listado de usuarios");
+		model.addAttribute("titulo", textoListar);
 		return "listar";
 	}
 	
