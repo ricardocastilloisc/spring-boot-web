7a9a5b643b60f8a5c3bc4a7385ad8ecdc4a93b72
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-06-27 12:33:32 -0500

    021 Obtener varios parametros de la URL con RequestParam

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/EjemploParamsController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/EjemploParamsController.java
@@ -1,5 +1,7 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,4 +17,28 @@ public class EjemploParamsController {
 		model.addAttribute("resultado", "El texto enviado es: " + texto);
 		return "params/ver";
 	}
+	
+	@GetMapping("/mix-params")
+	public String param(@RequestParam String saludo, @RequestParam Integer numero, Model model){
+		model.addAttribute("resultado", "El saludo enviado es: " + saludo + "y el número es :" + numero);
+		return "params/ver";
+	}
+	
+	@GetMapping("/mix-params-request")
+	public String param(HttpServletRequest request, Model model){
+		String saludo = request.getParameter("saludo");
+		Integer numero = null;
+		try {
+			numero = Integer.parseInt(request.getParameter("numero"));
+		} catch (Exception e) {
+			numero = 0;
+		}
+		model.addAttribute("resultado", "El saludo enviado es: " + saludo + "y el número es :" + numero);
+		return "params/ver";
+	}
+	
+	
+	
+	
+	
 }
